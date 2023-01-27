9f94c5d8612c973a3aa429c4a5bdf193c17fe24a
Emiliano <elmiliano41@gmail.com>
2023-01-27 00:12:22 -0600

    Table members styled

--- src/components/TableMembers.jsx
+++ src/components/TableMembers.jsx
@@ -11,6 +11,7 @@ import TablePagination from "@mui/material/TablePagination";
 import useTraining from "../hooks/useTraining";
 import EditIcon from "@mui/icons-material/Edit";
 import "../Styles/TableMembers.css";
+import { Row } from "react-bootstrap";
 
 function Tabla() {
   const navigate = useNavigate();
@@ -34,70 +35,70 @@ function Tabla() {
   const { members } = useTraining();
   return (
     <>
-      <div className="members-title">
-        <h1>Members</h1>
-      </div>
-      <div className="members-form">
-        <Paper sx={{ width: "100%", overflow: "hidden" }}>
-          {typeof members.length === "undefined" ? (
-            <h1> No found users </h1>
-          ) : (
-            <>
-              <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                  <TableHead>
-                    <TableRow>
-                      <TableCell>Name</TableCell>
-                      <TableCell>Email</TableCell>
-                      <TableCell>Member ID</TableCell>
-                      <TableCell>Role</TableCell>
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    {members
-                      .slice(
-                        page * rowsPerPage,
-                        page * rowsPerPage + rowsPerPage
-                      )
-                      .map((row) => (
-                        <TableRow
-                          onClick={() =>{handleCellClick(row)}}
-                          key={row.membersId}
-                          sx={{
-                            "&:last-child td, &:last-child th": { border: 0 },
-                          }}
-                        >
-                          <TableCell>
-                            {row.name +
-                              " " +
-                              row.firstName +
-                              " " +
-                              row.secondName}
-                          </TableCell>
-                          <TableCell>{row.email}</TableCell>
-                          <TableCell>{row.membersId}</TableCell>
-                          <TableCell>{row.adminId!=null&&row.mentorId!=null?"Admin, Mentor":row.adminId!=null?"Admin":row.mentorId!=null?"Mentor":"Colaborador"}</TableCell>
-                          <TableCell>
-                            <EditIcon>
-                            {row.membersId}</EditIcon>
-                          </TableCell>
-                        </TableRow>
-                      ))}
-                  </TableBody>
-                </Table>
-              </TableContainer>
-              <TablePagination
-                rowsPerPageOptions={[10, 25, 100]}
-                component="div"
-                count={members.length}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                onPageChange={handleChangePage}
-                onRowsPerPageChange={handleChangeRowsPerPage}
-              />
-            </>
-          )}
-        </Paper>
+      <div className="ContainerUserTable">
+        <div className="titleForm">Members</div>
+        <div className="members-form">
+          <Paper sx={{ width: "100%", overflow: "hidden" }}>
+            {typeof members.length === "undefined" ? (
+              <h1> No found users </h1>
+            ) : (
+              <>
+                <TableContainer component={Paper}>
+                  <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                    <TableHead>
+                      <TableRow>
+                        <TableCell>Name</TableCell>
+                        <TableCell>Email</TableCell>
+                        <TableCell>Member ID</TableCell>
+                        <TableCell>Role</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {members
+                        .slice(
+                          page * rowsPerPage,
+                          page * rowsPerPage + rowsPerPage
+                        )
+                        .map((row) => (
+                          <TableRow
+                            onClick={() => { handleCellClick(row) }}
+                            key={row.membersId}
+                            sx={{
+                              "&:last-child td, &:last-child th": { border: 0 },
+                            }}
+                          >
+                            <TableCell>
+                              {row.name +
+                                " " +
+                                row.lastName +
+                                " " +
+                                row.secondLastName}
+                            </TableCell>
+                            <TableCell>{row.email}</TableCell>
+                            <TableCell>{row.membersId}</TableCell>
+                            <TableCell>{row.adminId != null && row.mentorId != null ? "Admin, Mentor" : row.adminId != null ? "Admin" : row.mentorId != null ? "Mentor" : "Colaborador"}</TableCell>
+                            <TableCell>
+                              <EditIcon>
+                                {row.membersId}</EditIcon>
+                            </TableCell>
+                          </TableRow>
+                        ))}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+                <TablePagination
+                  rowsPerPageOptions={[10, 25, 100]}
+                  component="div"
+                  count={members.length}
+                  rowsPerPage={rowsPerPage}
+                  page={page}
+                  onPageChange={handleChangePage}
+                  onRowsPerPageChange={handleChangeRowsPerPage}
+                />
+              </>
+            )}
+          </Paper>
+        </div>
       </div>
     </>
   );
